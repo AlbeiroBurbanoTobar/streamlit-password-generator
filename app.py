fbd8668e348f24e861a3cab3ff9491cbd9f71b51
Albeiro Jose Burbano Tobar <139184498+AlbeiroBurbanoTobar@users.noreply.github.com>
2024-04-07 18:54:50 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import random
+import string
+
+# Título de la app
+st.title('Generador de contraseñas')
+st.write('Creado por Albeiro Burbano')
+
+# Solicitar la longitud de la contraseña
+longitud = st.number_input('¿Cuál será la longitud de la contraseña?', min_value=1, value=8, step=1)
+
+# Opciones de personalización
+incluye_numeros = st.checkbox('Incluir números')
+incluye_simbolos = st.checkbox('Incluir símbolos')
+
+# Generar la contraseña
+caracteres = string.ascii_letters
+if incluye_numeros:
+    caracteres += string.digits
+if incluye_simbolos:
+    caracteres += string.punctuation
+
+contrasena = ''.join(random.choice(caracteres) for i in range(longitud))
+st.write('Contraseña generada:', contrasena)
