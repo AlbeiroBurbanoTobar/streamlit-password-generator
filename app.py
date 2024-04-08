3e29dfff237693a56b61935616e172055ee7c066
Albeiro Jose Burbano Tobar <139184498+AlbeiroBurbanoTobar@users.noreply.github.com>
2024-04-07 19:01:13 -0500

    Update app.py
    volvemos a la versión vieja

--- app.py
+++ app.py
@@ -21,35 +21,5 @@ if incluye_simbolos:
     caracteres += string.punctuation
 
 contrasena = ''.join(random.choice(caracteres) for i in range(longitud))
-
-# Mostrar la contraseña y el botón para copiar
-st.write('Contraseña generada:')
-texto_copia = st.text_input('Haga clic en la contraseña para copiarla al portapapeles:', contrasena, key="contrasena", help="Haga clic en el texto para copiar")
-
-# JavaScript para habilitar la copia al portapapeles
-st.markdown("""
-    <script>
-    const copyToClipboard = str => {
-        const el = document.createElement('textarea');
-        el.value = str;
-        el.setAttribute('readonly', '');
-        el.style.position = 'absolute';
-        el.style.left = '-9999px';
-        document.body.appendChild(el);
-        el.select();
-        document.execCommand('copy');
-        document.body.removeChild(el);
-    };
-    const copyBtn = document.getElementById('copyButton');
-    copyBtn.addEventListener('click', function() {
-        copyToClipboard(document.getElementById('copyText').value);
-    });
-    </script>
-    """, unsafe_allow_html=True)
-
-# Botón para copiar al portapapeles
-if st.button('Copiar contraseña', key="copyButton"):
-    st.markdown(f'<input type="text" value="{contrasena}" id="copyText" style="position: absolute; left: -9999px;">', unsafe_allow_html=True)
-    st.markdown('<button id="copyButton">Copiar</button>', unsafe_allow_html=True)
-    st.success('Contraseña copiada al portapapeles')
+st.write('Contraseña generada:', contrasena)
 
