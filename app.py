30c22b773d0f7d88062500add434df34c0a8aef0
Albeiro Jose Burbano Tobar <139184498+AlbeiroBurbanoTobar@users.noreply.github.com>
2024-04-07 19:08:36 -0500

    Update app.py
    intentamos mejorar mas el diseño y funcionalidades

--- app.py
+++ app.py
@@ -2,25 +2,46 @@ import streamlit as st
 import random
 import string
 
-# Título de la app
-st.title('Generador de contraseñas')
-st.write('Creado por Albeiro Burbano')
+# Configurar la paleta de colores para los checkboxes
+st.markdown("""
+<style>
+div.row-widget.stCheckbox > div{flex-direction:row-reverse;}
+div.row-widget.stCheckbox > div > label{justify-content:space-between;}
+div.row-widget.stCheckbox > div > label > span{text-align:right;flex:1;}
+div.row-widget.stCheckbox > div > label > div{margin-left:-10px;}
+</style>
+""", unsafe_allow_html=True)
 
-# Solicitar la longitud de la contraseña
-longitud = st.number_input('¿Cuál será la longitud de la contraseña?', min_value=1, value=8, step=1)
+# Título de la app
+st.markdown('## Personalice su contraseña')
+
+# Personalización de la contraseña
+longitud = st.slider('Longitud de la contraseña', min_value=1, max_value=20, value=12, step=1)
 
 # Opciones de personalización
-incluye_numeros = st.checkbox('Incluir números')
-incluye_simbolos = st.checkbox('Incluir símbolos')
+mayusculas = st.checkbox('Mayúsculas', value=True)
+minusculas = st.checkbox('Minúsculas', value=True)
+numeros = st.checkbox('Números', value=True)
+simbolos = st.checkbox('Símbolos', value=True)
 
 # Generar la contraseña
-caracteres = string.ascii_letters
-if incluye_numeros:
+caracteres = ''
+if mayusculas:
+    caracteres += string.ascii_uppercase
+if minusculas:
+    caracteres += string.ascii_lowercase
+if numeros:
     caracteres += string.digits
-if incluye_simbolos:
+if simbolos:
     caracteres += string.punctuation
 
-contrasena = ''.join(random.choice(caracteres) for i in range(longitud))
+# Asegurar que se seleccione al menos una opción
+if not caracteres:
+    st.error('Por favor, seleccione al menos una opción para incluir en la contraseña.')
+else:
+    contrasena = ''.join(random.choice(caracteres) for i in range(longitud))
+    st.text_area('Contraseña generada', contrasena, height=50, key="contrasena")
+
 
 # Mostrar la contraseña en un campo de texto
 st.write('Contraseña generada:')
